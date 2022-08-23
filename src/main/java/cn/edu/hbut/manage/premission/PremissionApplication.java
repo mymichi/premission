45b23d151d53f6cb870fe53686c23a4277087367
lenovo <mymichi>
2022-08-23 11:04:24 +0800

    初次提交

---
+++ src/main/java/cn/edu/hbut/manage/premission/PremissionApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.hbut.manage.premission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/edu/hbut/manage/premission/PremissionApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.hbut.manage.premission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
